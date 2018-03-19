df79e375812d63897b45266f1eefccaad798893e
Matheus Hertzog <mihertzog@inf.ufpel.edu.br>
2018-03-18 21:32:23 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include "graphwidget.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    GraphWidget *widget = new GraphWidget;
+    QMainWindow mainWindow;
+    mainWindow.setCentralWidget(widget);
+
+    mainWindow.show();
 
     return a.exec();
 }
